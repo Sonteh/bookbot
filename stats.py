7791c4b3d6ae0040cecaffeb0b9855b176def32c
Sonteh <redwanc.gracjan@gmail.com>
2025-03-10 22:06:00 +0100

    Refactoring and cleaning up methods

--- stats.py
+++ stats.py
@@ -3,13 +3,22 @@ def get_words_count(text: str) -> int:
 
 def get_character_count(text: str) -> dict[str, int]:
     character_count = {}
-    words = text.split()
 
-    for word in words:
-        for character in word.lower():
-            if character in character_count:
-                character_count[character] = character_count[character] + 1
-            else:
-                character_count[character] = 1
+    for character in text.lower():
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
     
     return character_count
+
+def sort_dictionaries(dict: dict[str, int]) -> list[dict[str, int]]:
+    sorted_list_of_dicts = []
+    for key, value in dict.items():
+        sorted_list_of_dicts.append({"character": key, "count": value})
+
+    sorted_list_of_dicts.sort(reverse=True, key=sort_on)
+    return sorted_list_of_dicts
+
+def sort_on(dict: dict[str, int]) -> int:
+    return dict["count"]
